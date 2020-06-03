b60cfca974e43a5fcd3a0e9448b9d3cfeea4d0be
GeeKaven <xiaokang_show@live.com>
2020-06-03 22:34:42 +0800

    :sparkles: 27. 二叉树的镜像
    https://leetcode-cn.com/problems/er-cha-shu-de-jing-xiang-lcof/

---
+++ for-offer/27-二叉树的镜像/Lcof27.java
@@ -0,0 +1,34 @@
+public class Lcof27 {
+  public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+      val = x;
+    }
+  }
+
+  public TreeNode mirrorTree(TreeNode root) {
+    if (root == null) {
+      return null;
+    }
+    if (root.left == null && root.right == null) {
+      return root;
+    }
+    TreeNode temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+    if (root.left != null) {
+      mirrorTree(root.left);
+    }
+    if (root.right != null) {
+      mirrorTree(root.right);
+    }
+    return root;
+  }
+
+  public static void main(String[] args) {
+    
+  }
+}
