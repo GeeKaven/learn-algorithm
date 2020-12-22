a52e87f3fa17f7a98c3cef164b0dbf5db8423e93
geekaven <i@tawawa.moe>
2020-12-22 20:26:25 +0800

    feat(leetcode): 107_Binary_Tree_Level_Order_Traversal_II

--- code/JavaAlgorithm/src/main/java/moe/tawawa/leetcode/q107_Binary_Tree_Level_Order_Traversal_II/Solution.java
+++ code/JavaAlgorithm/src/main/java/moe/tawawa/leetcode/q107_Binary_Tree_Level_Order_Traversal_II/Solution.java
@@ -1,5 +1,40 @@
-package moe.tawawa.leetcode.q107_Binary_Tree_Level_Order_Traversal_II;/**
- * @author  GeeKaven
- * @date  2020/12/22 20:19
- */public class Solution {
+package moe.tawawa.leetcode.q107_Binary_Tree_Level_Order_Traversal_II;
+
+import model.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author GeeKaven
+ * @date 2020/12/22 20:19
+ */
+public class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> ans = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node == null) {
+                    continue;
+                }
+                level.add(node.val);
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+            ans.add(0, level);
+        }
+
+        return ans;
+    }
 }
