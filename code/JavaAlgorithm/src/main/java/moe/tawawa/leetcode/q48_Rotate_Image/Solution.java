9c15fe29a8439b97623b4a45764b4b100206958d
geekaven <i@tawawa.moe>
2021-02-22 23:37:00 +0800

    feat: 48_Rotate_Image

---
+++ code/JavaAlgorithm/src/main/java/moe/tawawa/leetcode/q48_Rotate_Image/Solution.java
@@ -0,0 +1,27 @@
+package moe.tawawa.leetcode.q48_Rotate_Image;
+
+/**
+ * @author GeeKaven
+ * @date 2021/2/22 23:30
+ */
+public class Solution {
+    // 水平反转+对角线反转
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - i - 1][j];
+                matrix[n - i - 1][j] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+}
