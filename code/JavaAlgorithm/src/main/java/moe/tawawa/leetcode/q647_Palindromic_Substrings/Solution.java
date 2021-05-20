0e6120971c2c2d05653cb89e1778104d988e3338
geekaven <i@tawawa.moe>
2021-05-20 22:20:37 +0800

    feat(leetcode): q647_Palindromic_Substrings

---
+++ code/JavaAlgorithm/src/main/java/moe/tawawa/leetcode/q647_Palindromic_Substrings/Solution.java
@@ -0,0 +1,23 @@
+package moe.tawawa.leetcode.q647_Palindromic_Substrings;
+
+/**
+ * @author GeeKaven
+ * @date 2021/5/20 22:12
+ */
+public class Solution {
+    public int countSubstrings(String s) {
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        int ans = 0;
+
+        for (int j = 0; j < s.length(); j++) {
+            for (int i = 0; i <= j; i++) {
+                if (s.charAt(i) == s.charAt(j) && (j - i < 2 || dp[i + 1][j - 1])) {
+                    dp[i][j] = true;
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
